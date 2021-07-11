d4c70e94309c624c8944f91b3de159db430d5c68
Ash <ashkanshahabi@gmail.com>
2021-07-11 16:04:16 +0100

    Update BurgerMenu's hover colour ref #9

--- Frontend/src/BurgerMenu.tsx
+++ Frontend/src/BurgerMenu.tsx
@@ -7,12 +7,12 @@ export default function BurgerMenu(props: {
 }) {
   const { className, open, onOpen } = props;
 
-  const divClassesCommon = `w-6 h-1 bg-white rounded-xl transition-all origin-1 `;
+  const divClassesCommon = `w-6 h-1 bg-white rounded-xl transition-all origin-1 group-hover:bg-blue-100`;
 
   return (
     <button
       onClick={() => onOpen(!open)}
-      className={`relative flex flex-col ml-5 justify-around w-6 h-6 bg-transparent  border-none cursor-pointer p-0 z-10 focus:outline-none ${className}`}
+      className={`relative flex flex-col ml-5 justify-around w-6 h-6 bg-transparent border-none cursor-pointer p-0 z-10 focus:outline-none group ${className}`}
     >
       <div
         className={`${divClassesCommon} transform ${
