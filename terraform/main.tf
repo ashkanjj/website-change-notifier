64544502d744c8aa370a7dcd885a7b2206dc59fc
Ash Shahabi <ashkanshahabi@gmail.com>
2022-04-09 20:27:42 +0100

    Added TF details to workflows

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,12 @@ terraform {
 
   required_version = ">= 0.14.9"
 
+  backend "s3" {
+    bucket = "website-change-notifier-tf-state"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+
 }
 
 
